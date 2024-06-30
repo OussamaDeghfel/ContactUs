c7a1f3f1d98fb8c15ff77de5ddf6ce279f7f8df5
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-06-30 11:24:13 +0100

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 function App() {
+  const [valide, setValide] = useState(false)
 
-
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   
+  const handleSubmit = (e) => {
+    if (emailRegex.test(e)) {
+      setValide(true)
+    } else {
+      setValide(false)
+    }
+  }
 
   return (
     <>
@@ -17,14 +25,14 @@ function App() {
                 <input
                   type="text"
                   id="firstName"
-                  className="cursor-pointer py-2 border-2 w-[262px] border-gray-400 rounded-md flex-wrap"
+                  className="p-2 cursor-pointer py-2 border-2 w-[262px] border-gray-400 rounded-md flex-wrap focus:outline-none focus:border-green-700"
                 />
               </div>
               <div className="flex flex-col ml-5">
                 <label className="py-2">Last Name</label>
                 <input
                   type="text"
-                  className="cursor-pointer py-2 border-2 w-[262px] border-gray-400 rounded-md flex-wrap"
+                  className="p-2 cursor-pointer py-2 border-2 w-[262px] border-gray-400 rounded-md flex- focus:outline-none focus:border-green-700"
                 />
               </div>
             </div>
@@ -32,7 +40,7 @@ function App() {
               <label className="py-2">Email Address</label>
               <input
                 type="email"
-                className=" py-2 border-2 w-full border-gray-400 rounded-md flex-wrap"
+                className="p-2 py-2 border-2 w-full border-gray-400 rounded-md flex-wrap focus:outline-none focus:border-green-700"
               />
             </div>
             <div className="mt-2">
@@ -44,8 +52,9 @@ function App() {
                     id="general"
                     name="general"
                     value="General-enquiry"
+                    className="accent-green-700"
                   />
-                  <label className="px-4">General Enquiry</label>
+                  <label className="px-4 ">General Enquiry</label>
                 </div>
                 <div className="py-2 px-6 border-2 w-[262px] border-gray-400 rounded-md flex-wrap cursor-pointer">
                   <input
@@ -53,6 +62,7 @@ function App() {
                     id="Support"
                     name="Support"
                     value="Support-request"
+                    className="accent-green-700"
                   />
                   <label className="px-4">Support Request</label>
                 </div>
@@ -66,16 +76,16 @@ function App() {
                   name="message"
                   rows="4"
                   cols="50"
-                  className="border-2 border-gray-400 rounded-md flex-wrap"
+                  className="p-2 border-2 border-gray-400 rounded-md flex-wrap focus:outline-none focus:border-green-700"
                 />
               </div>
               <div className="flex my-4">
-                <input type="checkbox" />
+                <input type="checkbox" className="accent-green-700" />
                 <label className="mx-2">
                   I consent to be contacted by the team{" "}
                 </label>
               </div>
-              <button className="w-full text-center bg-green-700 rounded-md text-white p-2 mt-2">
+              <button className="w-full text-center bg-green-700 rounded-md text-white p-2 mt-2 focus:outline-none focus:border-green-700">
                 Submit
               </button>
             </div>
