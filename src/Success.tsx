c6bb680e0b655a454516cbe1eb9df48ae2c0d30f
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-07-06 14:55:32 +0100

    add opacity

--- src/Success.tsx
+++ src/Success.tsx
@@ -4,9 +4,9 @@ import { FaCircleCheck } from 'react-icons/fa6'
 const Success = () => {
   return (
     <>
-     <div className='flex grid w-full h-[120px] bg-[#102231]  justify-center items-center p-4 rounded-md'>
-        <h1 className='flex font-bold items-center text-md text-white'> <FaCircleCheck /> Message Sent!</h1>
-        <p className='text-gray-400 mt-2'>Thanks for completing the form. We'll be in touch soon</p>
+     <div className='flex grid w-[450px] h-[100px] bg-[#102231]  justify-center items-center p-2 rounded-md opacity-95'>
+        <h1 className='flex font-bold items-center text-xl text-white'> <FaCircleCheck /> <span className='pl-2'> Message Sent! </span></h1>
+        <p className='text-gray-400'>Thanks for completing the form. We'll be in touch soon</p>
      </div>
     </>
   )
